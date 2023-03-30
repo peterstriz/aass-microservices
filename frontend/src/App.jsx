a6b29ac0dfe632c12f88f710cdcd26d2689f0483
Marek Vajda <marek.vajda009@gmail.com>
2023-03-30 19:13:53 +0200

    feat: frontend list kadernictiev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,8 +11,24 @@ function App() {
     });
   }
 
+  function HairsaloonList(props) {
+    const hairsaloons = props.hairsaloons;
+    const listItems = hairsaloons.map((hairsaloon) => (
+      <li key={hairsaloon.toString()}>
+        <a href="/">Kaderníctvo {hairsaloon}</a>
+      </li>
+    ));
+    return <ul>{listItems}</ul>;
+  }
+
+  // todo getnut nazvy hairsaloonov
+  // nejde mi naimportovat tu databazu a metadata do tej hasury :(
+  const hairsaloons = ['AMY', 'King-kong', 'Sigma-male'];
+
   return (
     <div className="App">
+      <h1>Vyberte si zo zoznamu kaderníctiev</h1>
+      <HairsaloonList hairsaloons={hairsaloons} />
       <button onClick={() => sendNotification()}>sendNotification()</button>
     </div>
   );
