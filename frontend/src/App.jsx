3925f066400537ef9d52ccaa71f628455e8537e7
Marek Vajda <marek.vajda009@gmail.com>
2023-03-30 19:41:06 +0200

    feat: pridanie datetime pickera

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import React, { useState } from 'react';
+import DateTimePicker from 'react-datetime-picker';
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
@@ -13,6 +15,12 @@ function App() {
 
   function HairsaloonList(props) {
     const hairsaloons = props.hairsaloons;
+    let selectedHairsaloon = '';
+
+    // const selectHairsaloon = () => {
+    //   // selectedHairsaloon = e.target.value;
+    //   alert('hehe');
+    // };
     const listItems = hairsaloons.map((hairsaloon) => (
       <li key={hairsaloon.toString()}>
         <a href="/">Kaderníctvo {hairsaloon}</a>
@@ -21,14 +29,26 @@ function App() {
     return <ul>{listItems}</ul>;
   }
 
+  function SelectDateTime(props) {
+    const [value, onChange] = useState(new Date());
+
+    return (
+      <div>
+        <DateTimePicker onChange={onChange} value={value} />
+      </div>
+    );
+  }
+
   // todo getnut nazvy hairsaloonov
   // nejde mi naimportovat tu databazu a metadata do tej hasury :(
   const hairsaloons = ['AMY', 'King-kong', 'Sigma-male'];
 
   return (
-    <div className="App">
+    <div className="App" style={{ fontFamily: 'arial' }}>
       <h1>Vyberte si zo zoznamu kaderníctiev</h1>
       <HairsaloonList hairsaloons={hairsaloons} />
+      <h1>Zvoľte termín</h1>
+      <SelectDateTime></SelectDateTime>
       <button onClick={() => sendNotification()}>sendNotification()</button>
     </div>
   );
